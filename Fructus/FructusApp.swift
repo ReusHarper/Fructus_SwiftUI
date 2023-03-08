7a91e08c05e1c8db108638395157f7147294d405
ReusHarper <reusharper@comunidad.unam.mx>
2023-03-08 16:28:27 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ReusHarper on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
